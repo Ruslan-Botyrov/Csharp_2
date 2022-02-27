522fe3def260e982b0601ef30868fc9120666ab8
Ruslan <botyrovra@gmail.com>
2022-02-27 11:25:42 +0300

    add task1

---
+++ task1/Program.cs
@@ -0,0 +1,8 @@
+﻿// Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает последнюю цифру этого числа.
+Console.Write("Введите трехзначное число");
+string inputNumber = Console.ReadLine();
+int number = int.Parse(inputNumber);
+if (number < 100) Console.Write()
+
+int x = number % 10;
+Console.Write (x);
