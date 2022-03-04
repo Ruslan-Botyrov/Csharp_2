c32857a44500994ed522e9429f643ed33215e1ef
Ruslan <botyrovra@gmail.com>
2022-03-04 22:24:02 +0300

    add homework3

---
+++ homework3/Program.cs
@@ -0,0 +1,10 @@
+﻿// Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+Console.Write("Введите число");
+string inputNumber = Console.ReadLine();
+int number = int.Parse(inputNumber);
+
+char[] arr = number.ToString().ToCharArray();
+
+if (number > 99) Console.WriteLine($"Третья цифра задонного числа - {arr[2]}");
+if (number < -99) Console.WriteLine($"Третья цифра задонного числа - {arr[3]}");
+else Console.WriteLine("Третьей цифры нет");
