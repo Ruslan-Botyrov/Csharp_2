a70bde1a5c41b9236f38e4d0c132b1a845eccc14
Ruslan <botyrovra@gmail.com>
2022-03-04 22:08:45 +0300

    update homework2v2

--- homework2v2/Program.cs
+++ homework2v2/Program.cs
@@ -4,6 +4,6 @@ Random rnd = new Random();
 int value = rnd.Next(100, 1000);
 Console.WriteLine(value);
 
-char[] arr = value.ToString().ToCharArray();
-arr (x)= arr[0,2];
-Console.WriteLine(x);
+char[] arr = value.ToString().ToCharArray(); // это преобразование числа в массив я нагуглил, просьба, разобрать данное преобразование на семинаре, а то не совсем его понимаю
+
+Console.WriteLine($"{arr[0]}{arr[2]}");
