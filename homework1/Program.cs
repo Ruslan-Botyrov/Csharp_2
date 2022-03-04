6e4bc140e5b0a991565a419daffebf46b708b5c2
Ruslan <botyrovra@gmail.com>
2022-03-04 21:04:48 +0300

    add homework1

---
+++ homework1/Program.cs
@@ -0,0 +1,6 @@
+﻿// Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+Console.Write("Введите трехзначное число");
+string inputNumber = Console.ReadLine();
+int number = int.Parse(inputNumber);
+int x = (number / 10) % 10;
+Console.WriteLine($"вторая цифра введенного числа - {x}");
